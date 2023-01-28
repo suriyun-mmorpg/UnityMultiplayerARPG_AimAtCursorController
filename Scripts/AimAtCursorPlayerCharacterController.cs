6c8b2058e822a900b150977dc3ef24469ad77983
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-01-28 07:43:10 +0700

    improve: improve codes follow core changes

--- Scripts/AimAtCursorPlayerCharacterController.cs
+++ Scripts/AimAtCursorPlayerCharacterController.cs
@@ -6,8 +6,6 @@ namespace MultiplayerARPG
 {
     public class AimAtCursorPlayerCharacterController : BasePlayerCharacterController
     {
-        public const float BUILDING_CONSTRUCTING_GROUND_FINDING_DISTANCE = 100f;
-
         [Header("Camera Controls Prefabs")]
         [SerializeField]
         protected FollowCameraControls gameplayCameraPrefab;
@@ -68,24 +66,43 @@ namespace MultiplayerARPG
         #endregion
 
         public byte HotkeyEquipWeaponSet { get; set; }
-        public FollowCameraControls CacheGameplayCameraControls { get; protected set; }
-        public FollowCameraControls CacheMinimapCameraControls { get; protected set; }
-        public Camera CacheGameplayCamera { get { return CacheGameplayCameraControls.CacheCamera; } }
-        public Camera CacheMiniMapCamera { get { return CacheMinimapCameraControls.CacheCamera; } }
-        public Transform CacheGameplayCameraTransform { get { return CacheGameplayCameraControls.CacheCameraTransform; } }
-        public Transform CacheMiniMapCameraTransform { get { return CacheMinimapCameraControls.CacheCameraTransform; } }
         public NearbyEntityDetector ActivatableEntityDetector { get; protected set; }
         public NearbyEntityDetector ItemDropEntityDetector { get; protected set; }
+        public IGameplayCameraController CacheGameplayCameraController { get; protected set; }
+        public IMinimapCameraController CacheMinimapCameraController { get; protected set; }
+
+        // Input & control states variables
         protected bool attackPreventedWhileCursorOverUI;
-        protected float buildYRotate;
 
         protected override void Awake()
         {
             base.Awake();
-            if (gameplayCameraPrefab != null)
-                CacheGameplayCameraControls = Instantiate(gameplayCameraPrefab);
-            if (minimapCameraPrefab != null)
-                CacheMinimapCameraControls = Instantiate(minimapCameraPrefab);
+            // Initial physic functions
+            if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
+                physicFunctions = new PhysicFunctions(512);
+            else
+                physicFunctions = new PhysicFunctions2D(512);
+            // Initial gameplay camera controller
+            CacheGameplayCameraController = gameObject.GetOrAddComponent<IGameplayCameraController, DefaultGameplayCameraController>((obj) =>
+            {
+                DefaultGameplayCameraController castedObj = obj as DefaultGameplayCameraController;
+                castedObj.SetData(gameplayCameraPrefab);
+            });
+            CacheGameplayCameraController.Init();
+            // Initial minimap camera controller
+            CacheMinimapCameraController = gameObject.GetOrAddComponent<IMinimapCameraController, DefaultMinimapCameraController>((obj) =>
+            {
+                DefaultMinimapCameraController castedObj = obj as DefaultMinimapCameraController;
+                castedObj.SetData(minimapCameraPrefab);
+            });
+            CacheMinimapCameraController.Init();
+            // Initial build aim controller
+            BuildAimController = gameObject.GetOrAddComponent<IBuildAimController, DefaultBuildAimController>((obj) =>
+            {
+                DefaultBuildAimController castedObj = obj as DefaultBuildAimController;
+                castedObj.SetData(buildGridSnap, buildGridOffsets, buildGridSize, buildRotationSnap, buildRotateAngle, buildRotateSpeed);
+            });
+            BuildAimController.Init();
             // Setup activate distance
             if (distanceToActivateByActivateKey <= 0f)
                 distanceToActivateByActivateKey = GameInstance.Singleton.conversationDistance;
@@ -102,31 +119,18 @@ namespace MultiplayerARPG
             ItemDropEntityDetector = tempGameObject.AddComponent<NearbyEntityDetector>();
             ItemDropEntityDetector.detectingRadius = distanceToActivateByPickupKey;
             ItemDropEntityDetector.findPickupActivatableEntity = true;
-            // Initial physic functions
-            if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-                physicFunctions = new PhysicFunctions(512);
-            else
-                physicFunctions = new PhysicFunctions2D(512);
         }
 
         protected override void Desetup(BasePlayerCharacterEntity characterEntity)
         {
             base.Desetup(characterEntity);
-
-            if (CacheGameplayCameraControls != null)
-                CacheGameplayCameraControls.target = null;
-
-            if (CacheMinimapCameraControls != null)
-                CacheMinimapCameraControls.target = null;
+            CacheGameplayCameraController.Desetup(characterEntity);
+            CacheMinimapCameraController.Desetup(characterEntity);
         }
 
         protected override void OnDestroy()
         {
             base.OnDestroy();
-            if (CacheGameplayCameraControls != null)
-                Destroy(CacheGameplayCameraControls.gameObject);
-            if (CacheMinimapCameraControls != null)
-                Destroy(CacheMinimapCameraControls.gameObject);
             if (ActivatableEntityDetector != null)
                 Destroy(ActivatableEntityDetector.gameObject);
             if (ItemDropEntityDetector != null)
@@ -138,11 +142,9 @@ namespace MultiplayerARPG
             if (!PlayingCharacterEntity || !PlayingCharacterEntity.IsOwnerClient)
                 return;
 
-            if (CacheGameplayCameraControls != null)
-                CacheGameplayCameraControls.target = CameraTargetTransform;
-
-            if (CacheMinimapCameraControls != null)
-                CacheMinimapCameraControls.target = CameraTargetTransform;
+            CacheGameplayCameraController.FollowingEntityTransform = CameraTargetTransform;
+            CacheMinimapCameraController.FollowingEntityTransform = CameraTargetTransform;
+            CacheMinimapCameraController.FollowingGameplayCameraTransform = CacheGameplayCameraController.CameraTransform;
 
             UpdateInput();
         }
@@ -279,7 +281,7 @@ namespace MultiplayerARPG
                 // NOTE: With this controller, single fire will do the same as automatic
                 if (InputManager.GetButtonDown("Fire1") || InputManager.GetButtonDown("Attack"))
                 {
-                    bool isPointerOverUIObject = CacheUISceneGameplay.IsPointerOverUIObject();
+                    bool isPointerOverUIObject = UISceneGameplay.IsPointerOverUIObject();
                     // If pointer over ui, start avoid attack inputs to prevent character attack while pointer over ui
                     if (isPointerOverUIObject)
                         attackPreventedWhileCursorOverUI = true;
@@ -327,7 +329,7 @@ namespace MultiplayerARPG
                         }
                     }
 
-                    bool isPointerOverUIObject = CacheUISceneGameplay.IsPointerOverUIObject();
+                    bool isPointerOverUIObject = UISceneGameplay.IsPointerOverUIObject();
                     // No pointer over ui and all attack key released, stop avoid attack inputs
                     if (attackPreventedWhileCursorOverUI && !isPointerOverUIObject)
                         attackPreventedWhileCursorOverUI = false;
@@ -386,7 +388,7 @@ namespace MultiplayerARPG
                     if (!tempGameEntity.IsNull())
                     {
                         foundTargetEntity = true;
-                        CacheUISceneGameplay.SetTargetEntity(tempGameEntity.Entity);
+                        UISceneGameplay.SetTargetEntity(tempGameEntity.Entity);
                         SelectedEntity = tempGameEntity.Entity;
                         if (tempGameEntity.Entity != PlayingCharacterEntity.Entity)
                         {
@@ -417,7 +419,7 @@ namespace MultiplayerARPG
                 Transform tempTransform;
                 IGameEntity tempGameEntity;
                 Vector3 tempTargetPosition;
-                int pickedCount = physicFunctions.RaycastPickObjects(CacheGameplayCamera, InputManager.MousePosition(), Physics.DefaultRaycastLayers, 100f, out _);
+                int pickedCount = physicFunctions.RaycastPickObjects(CacheGameplayCameraController.Camera, InputManager.MousePosition(), Physics.DefaultRaycastLayers, 100f, out _);
                 for (int i = pickedCount - 1; i >= 0; --i)
                 {
                     aimTargetPosition = physicFunctions.GetRaycastPoint(i);
@@ -426,7 +428,7 @@ namespace MultiplayerARPG
                     if (!tempGameEntity.IsNull())
                     {
                         foundTargetEntity = true;
-                        CacheUISceneGameplay.SetTargetEntity(tempGameEntity.Entity);
+                        UISceneGameplay.SetTargetEntity(tempGameEntity.Entity);
                         SelectedEntity = tempGameEntity.Entity;
                         if (tempGameEntity.Entity != PlayingCharacterEntity.Entity)
                         {
@@ -451,7 +453,7 @@ namespace MultiplayerARPG
             }
             if (!foundTargetEntity)
             {
-                CacheUISceneGameplay.SetTargetEntity(null);
+                UISceneGameplay.SetTargetEntity(null);
                 SelectedEntity = null;
             }
 
@@ -604,8 +606,8 @@ namespace MultiplayerARPG
             switch (CurrentGameInstance.DimensionType)
             {
                 case DimensionType.Dimension3D:
-                    Vector3 forward = CacheGameplayCameraControls.CacheCameraTransform.forward;
-                    Vector3 right = CacheGameplayCameraControls.CacheCameraTransform.right;
+                    Vector3 forward = CacheGameplayCameraController.CameraTransform.forward;
+                    Vector3 right = CacheGameplayCameraController.CameraTransform.right;
                     forward.y = 0f;
                     right.y = 0f;
                     forward.Normalize();
@@ -621,156 +623,6 @@ namespace MultiplayerARPG
                     break;
             }
             return moveDirection;
-        }
-
-        public override AimPosition UpdateBuildAimControls(Vector2 aimAxes, BuildingEntity prefab)
-        {
-            // Instantiate constructing building
-            if (ConstructingBuildingEntity == null)
-            {
-                InstantiateConstructingBuilding(prefab);
-                buildYRotate = 0f;
-            }
-            // Rotate by keys
-            Vector3 buildingAngles = Vector3.zero;
-            if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-            {
-                if (buildRotationSnap)
-                {
-                    if (InputManager.GetButtonDown("RotateLeft"))
-                        buildYRotate -= buildRotateAngle;
-                    if (InputManager.GetButtonDown("RotateRight"))
-                        buildYRotate += buildRotateAngle;
-                    // Make Y rotation set to 0, 90, 180
-                    buildingAngles.y = buildYRotate = Mathf.Round(buildYRotate / buildRotateAngle) * buildRotateAngle;
-                }
-                else
-                {
-                    float deltaTime = Time.deltaTime;
-                    if (InputManager.GetButton("RotateLeft"))
-                        buildYRotate -= buildRotateSpeed * deltaTime;
-                    if (InputManager.GetButton("RotateRight"))
-                        buildYRotate += buildRotateSpeed * deltaTime;
-                    // Rotate by set angles
-                    buildingAngles.y = buildYRotate;
-                }
-                ConstructingBuildingEntity.BuildYRotation = buildYRotate;
-            }
-            ConstructingBuildingEntity.Rotation = Quaternion.Euler(buildingAngles);
-            // Find position to place building
-            if (InputManager.useMobileInputOnNonMobile || Application.isMobilePlatform)
-                FindAndSetBuildingAreaByAxes(aimAxes);
-            else
-                FindAndSetBuildingAreaByMousePosition();
-            return AimPosition.CreatePosition(ConstructingBuildingEntity.Position);
-        }
-
-        public override void FinishBuildAimControls(bool isCancel)
-        {
-            if (isCancel)
-                CancelBuild();
-        }
-
-        public void FindAndSetBuildingAreaByAxes(Vector2 aimAxes)
-        {
-            Vector3 raycastPosition = EntityTransform.position + (GameplayUtils.GetDirectionByAxes(CacheGameplayCameraTransform, aimAxes.x, aimAxes.y) * ConstructingBuildingEntity.BuildDistance);
-            LoopSetBuildingArea(physicFunctions.RaycastDown(raycastPosition, CurrentGameInstance.GetBuildLayerMask()));
-        }
-
-        public void FindAndSetBuildingAreaByMousePosition()
-        {
-            LoopSetBuildingArea(physicFunctions.RaycastPickObjects(CacheGameplayCamera, InputManager.MousePosition(), CurrentGameInstance.GetBuildLayerMask(), Vector3.Distance(CacheGameplayCameraTransform.position, MovementTransform.position) + ConstructingBuildingEntity.BuildDistance, out _));
-        }
-
-        /// <summary>
-        /// Return true if found building area
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        protected bool LoopSetBuildingArea(int count)
-        {
-            ConstructingBuildingEntity.BuildingArea = null;
-            ConstructingBuildingEntity.HitSurface = false;
-            BuildingEntity buildingEntity;
-            BuildingArea buildingArea;
-            Transform tempTransform;
-            Vector3 tempRaycastPoint;
-            Vector3 snappedPosition = GetBuildingPlacePosition(ConstructingBuildingEntity.Position);
-            for (int tempCounter = 0; tempCounter < count; ++tempCounter)
-            {
-                tempTransform = physicFunctions.GetRaycastTransform(tempCounter);
-                if (ConstructingBuildingEntity.EntityTransform.root == tempTransform.root)
-                {
-                    // Hit collider which is part of constructing building entity, skip it
-                    continue;
-                }
-
-                tempRaycastPoint = physicFunctions.GetRaycastPoint(tempCounter);
-                snappedPosition = GetBuildingPlacePosition(tempRaycastPoint);
-
-                if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-                {
-                    // Find ground position from upper position
-                    bool hitAimmingObject = false;
-                    Vector3 raycastOrigin = tempRaycastPoint + Vector3.up * BUILDING_CONSTRUCTING_GROUND_FINDING_DISTANCE * 0.5f;
-                    RaycastHit[] groundHits = Physics.RaycastAll(raycastOrigin, Vector3.down, BUILDING_CONSTRUCTING_GROUND_FINDING_DISTANCE, CurrentGameInstance.GetBuildLayerMask());
-                    for (int j = 0; j < groundHits.Length; ++j)
-                    {
-                        if (groundHits[j].transform == tempTransform)
-                        {
-                            tempRaycastPoint = groundHits[j].point;
-                            snappedPosition = GetBuildingPlacePosition(tempRaycastPoint);
-                            ConstructingBuildingEntity.Position = GetBuildingPlacePosition(snappedPosition);
-                            hitAimmingObject = true;
-                            break;
-                        }
-                    }
-                    if (!hitAimmingObject)
-                        continue;
-                }
-                else
-                {
-                    ConstructingBuildingEntity.Position = GetBuildingPlacePosition(snappedPosition);
-                }
-
-                buildingEntity = tempTransform.root.GetComponent<BuildingEntity>();
-                buildingArea = tempTransform.GetComponent<BuildingArea>();
-                if ((buildingArea == null || !ConstructingBuildingEntity.BuildingTypes.Contains(buildingArea.buildingType))
-                    && buildingEntity == null)
-                {
-                    // Hit surface which is not building area or building entity
-                    ConstructingBuildingEntity.BuildingArea = null;
-                    ConstructingBuildingEntity.HitSurface = true;
-                    break;
-                }
-
-                if (buildingArea == null || !ConstructingBuildingEntity.BuildingTypes.Contains(buildingArea.buildingType))
-                {
-                    // Skip because this area is not allowed to build the building that you are going to build
-                    continue;
-                }
-
-                ConstructingBuildingEntity.BuildingArea = buildingArea;
-                ConstructingBuildingEntity.HitSurface = true;
-                return true;
-            }
-            ConstructingBuildingEntity.Position = GetBuildingPlacePosition(snappedPosition);
-            return false;
-        }
-
-        protected Vector3 GetBuildingPlacePosition(Vector3 position)
-        {
-            if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-            {
-                if (buildGridSnap)
-                    position = new Vector3(Mathf.Round(position.x / buildGridSize) * buildGridSize, position.y, Mathf.Round(position.z / buildGridSize) * buildGridSize) + buildGridOffsets;
-            }
-            else
-            {
-                if (buildGridSnap)
-                    position = new Vector3(Mathf.Round(position.x / buildGridSize) * buildGridSize, Mathf.Round(position.y / buildGridSize) * buildGridSize) + buildGridOffsets;
-            }
-            return position;
         }
 
         protected Vector2 XZ(Vector3 vector3)
